106af59ce3c605dddc47a8387098cbb17b4b1132
Martin Czygan <martin.czygan@gmail.com>
2016-01-25 16:28:55 +0100

    wip: a stripped down Entry

--- holdingfile.go
+++ holdingfile.go
@@ -1,6 +1,15 @@
 package holdingfile
 
-import "time"
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrBeforeCoverageInterval = errors.New("before coverage interval")
+	ErrAfterCoverageInterval  = errors.New("after coverage interval")
+	ErrMissingValues          = errors.New("missing values")
+)
 
 // Holdings can return a list of licenses for a given ISSN.
 type Holdings interface {
@@ -14,6 +23,75 @@ type License interface {
 	Covers(Signature) error
 	// TimeRestricted will report an error, if a moving wall constraint holds.
 	TimeRestricted(time.Time) error
+}
+
+// Entry is a reduced holding file entry.
+type Entry struct {
+	Begin   Signature
+	End     Signature
+	Embargo time.Duration
+}
+
+func (e Entry) Covers(s Signature) error {
+	if err := e.compareYear(s); err != nil {
+		return err
+	}
+	if err := e.compareVolume(s); err != nil {
+		return err
+	}
+	if err := e.compareIsse(s); err != nil {
+		return err
+	}
+	return nil
+}
+
+// compareYear returns an error, if both values are defined and disagree,
+// otherwise we assume there is no error.
+func (e Entry) compareYear(s Signature) error {
+	if e.Begin.Year != "" && s.Year != "" {
+		if s.Year < e.Begin.Year {
+			return ErrBeforeCoverageInterval
+		}
+	}
+	if e.End.Year != "" && s.Year != "" {
+		if s.Year > e.End.Year {
+			return ErrAfterCoverageInterval
+		}
+	}
+	return nil
+}
+
+// compareVolume returns an error, if both values are defined and disagree,
+// otherwise we assume there is no error.
+func (e Entry) compareVolume(s Signature) error {
+	if e.Begin.Volume == "" || s.Volume == "" {
+		return ErrMissingValues
+	}
+
+	if s.Volume < e.Begin.Volume {
+		return ErrBeforeCoverageInterval
+	} else {
+		if s.Volume > e.End.Volume {
+			return ErrAfterCoverageInterval
+		}
+	}
+	return nil
+}
+
+// compareIssue returns an error, if both values are defined and disagree,
+// otherwise we assume there is no error.
+func (e Entry) compareIssue(s Signature) error {
+	if e.Begin.Issue != "" && s.Issue != "" {
+		if s.Issue < e.Begin.Issue {
+			return ErrBeforeCoverageInterval
+		}
+	}
+	if e.End.Issue != "" && s.Issue != "" {
+		if s.Issue > e.End.Issue {
+			return ErrAfterCoverageInterval
+		}
+	}
+	return nil
 }
 
 // Signature is a bag of information of the record from which coverage can be
